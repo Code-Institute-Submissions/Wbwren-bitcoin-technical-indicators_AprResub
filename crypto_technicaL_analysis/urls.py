1806bdd10757ee11fed221837c6bba6eda7a286e
Wbwren <71226623+Wbwren@users.noreply.github.com>
2022-01-03 10:20:05 +0000

    Feat: added metrics views and templates

--- crypto_technicaL_analysis/urls.py
+++ crypto_technicaL_analysis/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('metrics/', include('metrics.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- metrics/views.py
+++ metrics/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Metric
 
 # Create your views here.
+
+def all_metrics(request):
+    """ A view to return all metrics """
+
+    metrics = Metric.objects.all()
+    context = {
+        'metrics': metrics
+        }
+
+    return render(request, 'metrics/metrics.html', context)
