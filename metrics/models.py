3e1d06529401f2c77eba2167b63656b7e980f219
Wbwren <71226623+Wbwren@users.noreply.github.com>
2022-01-07 11:28:12 +0000

    Docs: add docstrings to metrics app

--- metrics/models.py
+++ metrics/models.py
@@ -4,6 +4,7 @@ from django.db.models.fields import DecimalField
 from django.utils.translation import gettext as _
 
 # Create your models here.
+'''Model containing details of all bitcoin metrics'''
 class Metric(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -16,6 +17,7 @@ class Metric(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+'''Model containing bitcoin price data'''
 class Bitcoin_Price_Data(models.Model):
     date = models.DateField(_("date"))
     open = models.FloatField(_("open"))
@@ -23,11 +25,12 @@ class Bitcoin_Price_Data(models.Model):
     high = models.FloatField(_("high"))
     low = models.FloatField(_("low"))
 
+'''Model containing bitcoin price moving avergaes and risk metric data'''
 class Metric_Data(models.Model):
     date = models.DateField(_("date"))
     pi_cycle_top = models.FloatField(_("pi_cycle_top"), null=True, blank=True)
     ma200w = models.FloatField(_("200WMA"), null=True, blank=True)
-    ma50d_50w = models.FloatField(_("ma50D_50w"), null=True, blank=True)
+    ma50d_200d = models.FloatField(_("ma50d_200d"), null=True, blank=True)
     risk_indicator = models.FloatField(_("risk_indicator"), null=True, blank=True)
 
     def __str__(self):
--- metrics/views.py
+++ metrics/views.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 
 # Create your views here.
-
+'''View to display all metrics available'''
 def all_metrics(request):
     """ A view to return all metrics """
 
@@ -27,39 +27,26 @@ def all_metrics(request):
         }
     return render(request, 'metrics/metrics.html', context)
 
-
+'''View to display an individual metric'''
 def metric_detail(request, metric_id):
-    """ A view to show and individual metric """
-    """ 
-    View demonstrating how to display a graph object
-    on a web page with Plotly. 
-    """
-    
-    # temp = list(Bitcoin_Price_Data.objects.all())
-    
-    # for item in temp:
-    #     print(item.headline)
-    conn = sqlite3.connect('db.sqlite3')
-    print('conn')
-          
+    # Connect to database and retrieve bitcoin price data
+    conn = sqlite3.connect('db.sqlite3')      
     sql_query = pd.read_sql_query('''SELECT * FROM metrics_bitcoin_price_data''', conn)
-
     df = pd.DataFrame(sql_query, columns = ['date', 'open', 'price', 'high', 'low'])
-    
 
     # Calculate moving averages using dataframe
     df['111DMA'] = df.iloc[:,1].rolling(window=111).mean()
     df['350DMA*2'] = (df.iloc[:,1].rolling(window=350).mean())*2
-    df['200WMA'] = df.iloc[:,1].rolling(window=200).mean()
+    df['200WMA'] = df.iloc[:,1].rolling(window=1400).mean()
     df['50DMA'] = df.iloc[:,1].rolling(window=50).mean()
-    df['350DMA'] = df.iloc[:,1].rolling(window=350).mean()
-
+    df['200DMA'] = df.iloc[:,1].rolling(window=200).mean()
 
     fig = px.line(df, x='date', y=df['price'])
     
+    # Check which metric is selected and add the appropritate moving averages
     if int(metric_id) == 3:
         fig.add_scatter(name='50DMA', x=df['date'], y=df['50DMA'])
-        fig.add_scatter(name='350DMA', x=df['date'], y=df['350DMA'])
+        fig.add_scatter(name='200DMA', x=df['date'], y=df['200DMA'])
     elif int(metric_id) == 4:
         fig.add_scatter(name='200WMA', x=df['date'], y=df['200WMA'])
     elif int(metric_id) == 5:
@@ -70,18 +57,7 @@ def metric_detail(request, metric_id):
         fig.add_vline(x='2013-04-05', line_dash="dash", line_width=2, line_color="green")
         fig.add_vline(x='2021-04-11', line_dash="dash", line_width=2, line_color="green")
 
-    # fig.update_layout(
-    #     width=1600, height=800,
-    #     title='Bitcoin Data',
-    #     yaxis_title='Price',
-    #     shapes = [dict(
-    #         x0='2012-12-09', x1='2012-12-09', y0=0, y1=1, xref='x', yref='paper',
-    #         line_width=2)],
-    #     annotations=[dict(
-    #         x='2016-12-09', y=0.05, xref='x', yref='paper',
-    #         showarrow=False, xanchor='left', text='Increase Period Begins')]
-    # )
-
+    # Set the y axis to a logarithmic scale
     fig.update_yaxes(type="log", range=[-1,5])
 
     # Getting HTML needed to render the plot.
