726a5f0aeee6b55e53355f1b301f7a60dd851d10
Wbwren <71226623+Wbwren@users.noreply.github.com>
2022-01-12 13:35:25 +0000

    Feat: implement admin add price data

--- admincrud/urls.py
+++ admincrud/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.admincrud, name='admincrud'),
-    path('delete-<date>', views.delete_date, name='delete_date'),
+    path('add_price_data/', views.add_price_data, name='add_price_data'),
+    path('delete/<date>', views.delete_date, name='delete_date'),
 ]
--- admincrud/views.py
+++ admincrud/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.contrib import messages
+from django import forms
 from metrics.models import Bitcoin_Price_Data
+from .forms import BitcoinAddForm
+
 
 
 # Create your views here.
@@ -18,6 +22,22 @@ def delete_date(request, date):
     get_object_or_404(Bitcoin_Price_Data, date=date).delete()
     return redirect(reverse('admincrud'))
 
-# def edit_date(request, date):
-#     get_object_or_404(Bitcoin_Price_Data, date=date).delete()
-#     return render(request, 'admincrud/admincrud.html')
+def add_price_data(request):
+    """ Add new price data """
+    if request.method == 'POST':
+        form = BitcoinAddForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added date!')
+            return redirect(reverse('add_price_data'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = BitcoinAddForm()
+        
+    template = 'admincrud/add_price_data.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
