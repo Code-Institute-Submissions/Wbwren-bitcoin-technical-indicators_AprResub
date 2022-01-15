ae9533963571967d13e4581f58586734d82e8c13
Wbwren <71226623+Wbwren@users.noreply.github.com>
2022-01-15 12:46:37 +0000

    Fix: path issue with heroku deployment

--- bitcoin_technical_indicators/settings.py
+++ bitcoin_technical_indicators/settings.py
@@ -16,13 +16,14 @@ import os
 import dj_database_url
 
 
-env = environ.Env()
-environ.Env.read_env()
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, 'bitcoin_technical_indicators/.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
